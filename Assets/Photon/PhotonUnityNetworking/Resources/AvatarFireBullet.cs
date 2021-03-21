c918cc81e89712850ccdd7ba20746ebe18da0f1a
sbubu12 <shihoyokoi@gmail.com>
2021-03-21 22:00:42 +0900

    Bulletアタッチできない

---
+++ Assets/Photon/PhotonUnityNetworking/Resources/AvatarFireBullet.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class AvatarFireBullet : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Bullet bulletPrefab = default;
+
+    private void Update()
+    {
+        if (photonView.IsMine)
+        {
+            // 左クリックでカーソルの方向に弾を発射する
+            if (Input.GetMouseButtonDown(0))
+            {
+                var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                var direction = mousePosition - transform.position;
+                float angle = Mathf.Atan2(direction.y, direction.x);
+
+                FireBullet(angle);
+            }
+        }
+    }
+
+    // 弾を発射するメソッド
+    private void FireBullet(float angle)
+    {
+        var bullet = Instantiate(bulletPrefab);
+        bullet.Init(transform.position, angle);
+    }
+}
