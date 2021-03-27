9745d0450583ec7d995a98d071ec7f79509fd27b
sbubu12 <shihoyokoi@gmail.com>
2021-03-27 14:56:34 +0900

    弾の同期がされない

--- Assets/Photon/PhotonUnityNetworking/Resources/AvatarFireBullet.cs
+++ Assets/Photon/PhotonUnityNetworking/Resources/AvatarFireBullet.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 public class AvatarFireBullet : MonoBehaviourPunCallbacks
 {
     [SerializeField]
-    private Bullet bulletPrefab = default;
+    private GameObject bulletPrefab = default;
+
+    private int nextBulletId = 0;
+
+    public void Start()
+    {
+       
+    }
 
     private void Update()
     {
@@ -17,15 +24,17 @@ public class AvatarFireBullet : MonoBehaviourPunCallbacks
                 var direction = mousePosition - transform.position;
                 float angle = Mathf.Atan2(direction.y, direction.x);
 
-                FireBullet(angle);
+                photonView.RPC(nameof(FireBullet), RpcTarget.All, nextBulletId++, angle);
             }
         }
     }
 
     // 弾を発射するメソッド
-    private void FireBullet(float angle)
+    [PunRPC]
+    private void FireBullet(int id,float angle)
     {
-        var bullet = Instantiate(bulletPrefab);
-        bullet.Init(transform.position, angle);
+        GameObject bullet = Instantiate(bulletPrefab);
+        bullet.GetComponent<Bullet>().Init(id,photonView.OwnerActorNr,transform.position, angle);
+
     }
 }
--- Assets/Photon/PhotonUnityNetworking/Resources/Bullet.cs
+++ Assets/Photon/PhotonUnityNetworking/Resources/Bullet.cs
@@ -4,6 +4,23 @@ public class Bullet : MonoBehaviour
 {
     private Vector3 velocity;
 
+    //弾のIDを返すプロパティ
+    public int Id { get; private set; }
+    //弾を発射したプレイヤーのIDを返すプロパティ
+    public int OwnerId { get; private set; }
+
+    //同じ弾かどうかをIDで判定するメソッド
+    public bool Equals(int id, int ownerId) => id == Id && ownerId == OwnerId;
+
+    public void Init(int id, int ownerId, Vector3 origin, float angle)
+    {
+        Id = id;
+        OwnerId = ownerId;
+        transform.position = origin;
+        velocity = 9f * new Vector3(Mathf.Cos(angle), Mathf.Sin(angle));
+    }
+
+    
     public void Init(Vector3 origin, float angle)
     {
         transform.position = origin;
